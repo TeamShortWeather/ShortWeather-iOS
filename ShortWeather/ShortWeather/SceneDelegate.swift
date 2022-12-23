77f561f0c9de8f19782d9174de65b377f0703a06
유서린 <sprout3082@gmail.com>
2022-12-23 17:49:03 +0900

    [Setting] #1 - Storyboard 없애기 ( Code Base )

--- ShortWeather/ShortWeather/SceneDelegate.swift
+++ ShortWeather/ShortWeather/SceneDelegate.swift
@@ -17,6 +17,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let windowScene = scene as? UIWindowScene {
+            
+            let window = UIWindow(windowScene: windowScene)
+            window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
+            
+            let rootVC = ViewController()
+            let navigationController = UINavigationController(rootViewController: rootVC)
+            
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+            self.window = window
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
