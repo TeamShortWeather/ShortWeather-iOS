ba3ad56f85a7cff54bb3636ff0f1a4fe5dad114c
유서린 <sprout3082@gmail.com>
2022-12-29 17:39:46 +0900

    [Feat] #1 - Color Setting

---
+++ ShortWeather/ShortWeather/Util/NameSpace/Color.swift
@@ -0,0 +1,47 @@
+//
+//  Color.swift
+//  ShortWeather
+//
+//  Created by Mac Book Pro on 2022/12/29.
+//
+
+import UIKit
+
+public enum Color {
+
+    //MARK: - Achromatic Color
+    
+    public static let black = UIColor(named: "black")!
+    public static let gray8 = UIColor(named: "gray8")!
+    public static let gray7 = UIColor(named: "gray7")!
+    public static let gray6 = UIColor(named: "gray6")!
+    public static let gray5 = UIColor(named: "gray5")!
+    public static let gray4 = UIColor(named: "gray4")!
+    public static let gray3 = UIColor(named: "gray3")!
+    public static let gray2 = UIColor(named: "gray2")!
+    public static let gray1 = UIColor(named: "gray1")!
+    public static let gray0 = UIColor(named: "gray0")!
+    public static let white = UIColor(named: "white")!
+    
+    //MARK: - Main Color
+    
+    public static let mainColor = UIColor(named: "mainColor")!
+    public static let pointColor = UIColor(named: "pointColor")!
+    
+    //MARK: - Weather Color
+    
+    public static let weatherSkyblue1 = UIColor(named: "weatherSkyblue1")!
+    public static let weatherSkyblue2 = UIColor(named: "weatherSkyblue2")!
+    public static let weatherBlue = UIColor(named: "weatherBlue")!
+    public static let dustGood1 = UIColor(named: "dustGood1")!
+    public static let dustGood2 = UIColor(named: "dustGood2")!
+    public static let dustNormal1 = UIColor(named: "dustNormal1")!
+    public static let dustNormal2 = UIColor(named: "dustNormal2")!
+    public static let weatherYellow = UIColor(named: "weatherYellow")!
+    public static let weatherOrange = UIColor(named: "weatherOrange")!
+    public static let dustBad1 = UIColor(named: "dustBad1")!
+    public static let dustBad2 = UIColor(named: "dustBad2")!
+    public static let dustWorst1 = UIColor(named: "dustWorst1")!
+    public static let dustWorst2 = UIColor(named: "dustWorst2")!
+    public static let weatherPurple = UIColor(named: "weatherPurple")!
+}
