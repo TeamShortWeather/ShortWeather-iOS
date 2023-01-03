7447eaae08e650f255f09c4b68dc50239c22413f
유서린 <sprout3082@gmail.com>
2023-01-03 15:37:44 +0900

    [Feat] #4 - TableView, CollectionView extension

---
+++ ShortWeather/ShortWeather/Util/NameSpace/Letter.swift
@@ -0,0 +1,18 @@
+//
+//  Letters.swift
+//  ShortWeather
+//
+//  Created by Mac Book Pro on 2023/01/03.
+//
+
+import Foundation
+
+public enum Letter {
+
+    //MARK: - Tab Title
+    public static let todayWeather = "오늘 날씨"
+    public static let weekWeather = "주간 날씨"
+    public static let cloth = "옷"
+    public static let setting = "설정"
+    
+}
