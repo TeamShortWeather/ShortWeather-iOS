2c79198bdcc08ab1b7ff72f1dafb81386cf7f9a0
kwonjeong <kwonjeong9470@gmail.com>
2023-01-07 21:18:48 +0900

    [Design] #7 -  CheckButton CustomClass 구현

--- ShortWeather/ShortWeather/Util/CustomClass/CheckButton.swift
+++ ShortWeather/ShortWeather/Util/CustomClass/CheckButton.swift
@@ -5,19 +5,53 @@
 //  Created by KJ on 2023/01/07.
 //
 
+enum ButtonState {
+    case allow
+    case notAllow
+}
+
 import UIKit
 
 class CheckButton: UIButton {
     
-//    private let buttonName: String
-//    private let buttonState: String
+    private let buttonName: String
+    private let buttonState: ButtonState
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
+    init(buttonName: String, buttonState: ButtonState) {
+        self.buttonName = buttonName
+        self.buttonState = buttonState
+        super.init(frame: .zero)
+        setUIButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUIButton() {
+        setTitle(buttonName, for: .normal)
+        layer.cornerRadius = 15
+        titleLabel?.font = .fontGuide(.subhead2)
+        switch buttonState {
+        case .allow:
+            allowButton()
+        case.notAllow:
+            notAllowButton()
+        }
+    }
+    
+    func allowButton() {
+        setTitleColor(Color.white, for: .normal)
+        backgroundColor = Color.pointColor
+    }
+    
+    func notAllowButton() {
+        setTitleColor(Color.black, for: .normal)
+        backgroundColor = Color.gray2
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = UIScreen.main.bounds.width - 56
+        return CGSize(width: size, height: 57)
     }
 }
