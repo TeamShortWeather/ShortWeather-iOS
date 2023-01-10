34541ba3434ddf82cf6d218f7d0b5a74e3af085a
kwonjeong <kwonjeong9470@gmail.com>
2023-01-10 23:23:23 +0900

    [Fix] #46 - ListInfoView 생성

---
+++ ShortWeather/ShortWeather/Presentation/InfoSetting/Common/View/ListInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  ListInfoView.swift
+//  ShortWeather
+//
+//  Created by KJ on 2023/01/10.
+//
+
+import UIKit
+
+class ListInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
