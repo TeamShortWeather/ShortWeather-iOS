ebd7707e8098f8e2e320636260e3a7c4b69112e1
유서린 <sprout3082@gmail.com>
2023-01-11 00:57:47 +0900

    [Feat] #26 - Int extension Temperature

---
+++ ShortWeather/ShortWeather/Util/Extension/Int +.swift
@@ -0,0 +1,15 @@
+//
+//  Int +.swift
+//  ShortWeather
+//
+//  Created by Mac Book Pro on 2023/01/11.
+//
+
+import Foundation
+
+extension Int {
+    
+    var temperature: String {
+        return "\(self)˚"
+    }
+}
