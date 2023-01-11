8d2ad40b64dd1ecda48e17c2d2271da16af8afba
Mini <minnnidev@gmail.com>
2023-01-11 18:10:12 +0900

    [Feat] #53 - TodayWeatherDetailService 구현

--- ShortWeather/ShortWeather/Network/APIService/TodayWeatherDetail/TodayWeatherDetailService.swift
+++ ShortWeather/ShortWeather/Network/APIService/TodayWeatherDetail/TodayWeatherDetailService.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import Moya
+
+enum TodayWeatherDetailService {
+    case fetchDetailTemp
+    case fetchDetailRain
+}
+
+extension TodayWeatherDetailService: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConst.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchDetailTemp:
+            return URLConst.getDetailTempTodayWeatherURL
+        case .fetchDetailRain:
+            return URLConst.getDetailRainTodayWeatherURL
+            
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .fetchDetailTemp:
+            return .get
+        case .fetchDetailRain:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .fetchDetailTemp:
+            return .requestPlain
+        case .fetchDetailRain:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .fetchDetailTemp:
+            return APIConstants.headerWithNoToken
+        case .fetchDetailRain:
+            return APIConstants.headerWithNoToken
+        }
+    }
+    
+    
+}
