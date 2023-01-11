b29c35ca0a57bea7809baa2df4e7ba69e2b40b2e
유서린 <sprout3082@gmail.com>
2023-01-11 16:20:43 +0900

    [Feat] #50 - Server 통신 세팅

--- ShortWeather/ShortWeather/Network/Foundation/URLConst.swift
+++ ShortWeather/ShortWeather/Network/Foundation/URLConst.swift
@@ -9,5 +9,19 @@ import Foundation
 
 public enum URLConst {
     
+    //MARK: - Base URL
+    
     public static let baseURL = "54.180.113.172:8000"
+    
+    //MARK: - URL Path
+    
+    public static let checkUserURL = "/auth/login"
+    public static let postUserURL = "/auth"
+    public static let getTodayWeatherURL = "/weather/today"
+    public static let getTodayWeatherQuestionURL = "/weather/today/question"
+    public static let getDetailTodayWeatherURL = "/weather/today/detail"
+    public static let getDetailTempTodayWeatherURL = "/weather/today/detail/temp"
+    public static let getDetailRainTodayWeatherURL = "/weather/today/detail/rain"
+    public static let postSetWakeupURL = "/setting/wakeup"
+    public static let postSetCommuteURL = "/setting/commute"
 }
