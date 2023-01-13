1c906d73cc09309f7712b0a6464b383ab87a41e0
kwonjeong <kwonjeong9470@gmail.com>
2023-01-13 22:51:36 +0900

    [Fix] #78 - 주간날씨 레이아웃 조정

--- ShortWeather/ShortWeather/Presentation/WeekWeather/View/WeekWeatherView.swift
+++ ShortWeather/ShortWeather/Presentation/WeekWeather/View/WeekWeatherView.swift
@@ -46,6 +46,10 @@ extension WeekWeatherView {
     // MARK: - UI Components Property
     
     private func setUI() {
+        weekDescriptionView.do {
+            $0.backgroundColor = .white
+        }
+        
         dailyForecastLabel.do {
             $0.text = "일별예보"
             $0.font = .fontGuide(.caption1)
@@ -68,7 +72,6 @@ extension WeekWeatherView {
             $0.registerCell(WeekWeatherCollectionViewCell.self)
             $0.showsVerticalScrollIndicator = false
             $0.backgroundColor = .clear
-            $0.register(WeekWeatherHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: WeekWeatherHeaderView.identifier)
         }
     }
     
@@ -76,32 +79,32 @@ extension WeekWeatherView {
     
     private func setLayout() {
         weekDescriptionView.addSubviews(dailyForecastLabel, timeZoneLabel, temperatureLabel)
-        addSubviews(weekWeatherCollectionView)
+        addSubviews(weekWeatherCollectionView, weekDescriptionView)
         
         weekDescriptionView.snp.makeConstraints {
             $0.top.equalToSuperview()
-            $0.leading.equalToSuperview()
-            $0.centerX.equalToSuperview()
-            $0.height.equalTo(18)
+            $0.leading.equalToSuperview().offset(36)
+            $0.trailing.equalToSuperview().inset(34)
+            $0.height.equalTo(38)
         }
         
         dailyForecastLabel.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
+            $0.top.equalToSuperview().offset(16)
             $0.leading.equalToSuperview()
         }
         
         timeZoneLabel.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
+            $0.top.equalTo(dailyForecastLabel)
             $0.centerX.equalToSuperview()
         }
         
         temperatureLabel.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(8)
-            $0.centerY.equalToSuperview()
+            $0.top.equalTo(dailyForecastLabel)
+            $0.trailing.equalToSuperview()
         }
         
         weekWeatherCollectionView.snp.makeConstraints {
-            $0.top.equalToSuperview()
+            $0.top.equalTo(weekDescriptionView.snp.bottom)
             $0.leading.equalToSuperview().offset(28)
             $0.trailing.equalToSuperview().inset(28)
             $0.bottom.equalToSuperview().inset(20)
@@ -129,16 +132,6 @@ extension WeekWeatherView: UICollectionViewDataSource {
         cell.setDataBind(model: weatherWeekModel[indexPath.row])
         return cell
     }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        guard kind == UICollectionView.elementKindSectionHeader,
-              let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                           withReuseIdentifier: WeekWeatherHeaderView.identifier,
-                                                                           for: indexPath) as? WeekWeatherHeaderView else {
-            return UICollectionReusableView()
-        }
-        return header
-    }
 }
 
 //MARK: - UICollectionViewDelegateFlowLayout
@@ -148,14 +141,5 @@ extension WeekWeatherView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = bounds.size.width - 56
         return CGSize(width: width, height: 72)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        let width = collectionView.frame.width
-        return CGSize(width: width, height: 18)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-       return UIEdgeInsets(top: 4, left: 0, bottom: 0, right: 0)
     }
 }
